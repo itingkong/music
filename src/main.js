1d341144779422fce72e776f3890093c3474ebdb
itingkong <luoqi20131001@163.com>
2018-05-25 19:07:26 +0800

    simple modified

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
+import fastclick from 'fastclick'
+// import VueLazyload from 'vue-lazyload'
+// import store from './store'
 
-Vue.config.productionTip = false
+// Vue.config.productionTip = false
+
+fastclick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
