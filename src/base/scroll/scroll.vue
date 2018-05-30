e7f6f3878db7d53d3b6ee0bed673a44150df4e2f
itingkong <luoqi20131001@163.com>
2018-05-30 14:13:39 +0800

    scroll组件

---
+++ src/base/scroll/scroll.vue
@@ -0,0 +1,97 @@
+<template>
+  <div ref="wrap">
+    <slot></slot>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+import BScroll from 'better-scroll'
+
+export default {
+  props: {
+    probeType: {
+      type: Number,
+      default: 1
+    },
+    click: {
+      type: Boolean,
+      default: true
+    },
+    data: {
+      type: Array,
+      default: null
+    },
+    listenScroll: {
+      type: Boolean,
+      default: false
+    },
+    pull: {
+      type: Boolean,
+      default: false
+    },
+    beforeScroll: {
+      type: Boolean,
+      default: false
+    },
+    refreshDelay: {
+      type: Number,
+      default: 20
+    }
+  },
+  mounted () {
+
+  },
+  methods: {
+    _initScroll () {
+      if (!this.$refs.wrap) {
+        return
+      }
+
+      this.scroll = new BScroll(this.$refs.wrap, {
+        probeType: this.probeType,
+        click: this.click
+      })
+
+      if (this.listenScroll) {
+        let self = this
+        this.scroll.on('scroll', (pos) => {
+          self.$emit('scroll', pos)
+        })
+      }
+
+      if (this.pullup) {
+        this.scroll.on('scrollEnd', () => {
+          if (this.scroll.y <= (this.scroll.maxScrollY + 50)) {
+            this.$emit('scrollEnd')
+          }
+        })
+      }
+
+      if (this.beforeScroll) {
+        this.scroll.on('beforeScrollStart', () => {
+          this.$emit('beforeScrollStart')
+        })
+      }
+    },
+    disable () {
+      this.scroll && this.scroll.disable()
+    },
+    enable () {
+      this.scroll && this.scroll.enable()
+    },
+    refresh () {
+      this.scroll && this.scroll.refresh()
+    },
+    scrollTo () {
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+    },
+    scrollToElement () {
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,9 +1,17 @@
 <template>
-  <div class="wrap">recommend</div>
+  <div class="recommend" ref="recommend">
+    <scroll ref="scroll" class="recommend-content"></scroll>
+  </div>
 </template>
 
 <script>
-export default{}
+import Scroll from 'base/scroll/scroll'
+
+export default{
+  components: {
+    Scroll
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
