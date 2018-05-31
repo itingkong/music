242400ae85e6c5f8eb5bab9619c77b7ccfff562d
itingkong <luoqi20131001@163.com>
2018-05-31 15:43:34 +0800

    轮播图

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="slider" ref="slider">
     <div class="slider-group" ref="sliderGroup">
-      <slot></slot>
+      <slot>
+      </slot>
     </div>
     <div class="dots">
-      <span class="dot"></span>
+      <span class="dot" :class="{active: currentPageIndex === index }" v-for="(item, index) in dots" :key="index"></span>
     </div>
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
 import {addClass} from 'common/js/dom'
 import BScroll from 'better-scroll'
-// import
 
 export default {
   name: 'slider',
@@ -46,30 +46,43 @@ export default {
         this._play()
       }
     }, 20)
+
+    window.addEventListener('resize', () => {
+      if (!this.slider) {
+        return
+      }
+      this._setSliderWidth(true)
+      this.slider.refresh()
+    })
+  },
+  activated () {
+    if (this.autoPlay) {
+      this._play()
+    }
+  },
+  deactivated () {
+    clearTimeout(this.timer)
+  },
+  beforeDestroy () {
+    clearTimeout(this.timer)
   },
   methods: {
     _setSliderWidth (isResize) {
       this.children = this.$refs.sliderGroup.children
 
       let width = 0
-      let sliderWidth = this.$refs.slider.children
-
-      for (let i = 0; i < this.children; i++) {
+      let sliderWidth = this.$refs.slider.clientWidth
+      for (let i = 0; i < this.children.length; i++) {
         let child = this.children[i]
+        addClass(child, 'slider-item')
+
         child.style.width = sliderWidth + 'px'
         width += sliderWidth
-
-        addClass(child, 'slider-item')
       }
-
       if (this.loop && !isResize) {
-        width += sliderWidth * 2
+        width += 2 * sliderWidth
       }
-
       this.$refs.sliderGroup.style.width = width + 'px'
-    },
-    _initDots () {
-      this.dots = new Array(this.children.length)
     },
     _initSlider () {
       this.slider = new BScroll(this.$refs.slider, {
@@ -79,7 +92,7 @@ export default {
         snap: true,
         snapLoop: this.loop,
         snapThreshold: 0.3,
-        snapSpeen: 400
+        snapSpeed: 400
       })
 
       this.slider.on('scrollEnd', () => {
@@ -87,7 +100,6 @@ export default {
         if (this.loop) {
           pageIndex -= 1
         }
-
         this.currentPageIndex = pageIndex
 
         if (this.autoPlay) {
@@ -101,12 +113,14 @@ export default {
         }
       })
     },
+    _initDots () {
+      this.dots = new Array(this.children.length)
+    },
     _play () {
       let pageIndex = this.currentPageIndex + 1
       if (this.loop) {
         pageIndex += 1
       }
-
       this.timer = setTimeout(() => {
         this.slider.goToPage(pageIndex, 0, 400)
       }, this.interval)
@@ -115,45 +129,44 @@ export default {
 }
 </script>
 
-<style lang="stylus" scoped>
+<style scoped lang="stylus" rel="stylesheet/stylus">
 @import "~common/stylus/variable"
 
 .slider
-  min-height: 1px
+  min-height 1px
   .slider-group
-    position: relative
-    overflow: hidden
-    white-space: nowrap
+    position relative
+    overflow hidden
+    white-space nowrap
     .slider-item
-      float: left
-      box-sizing: border-box
-      overflow: hidden
-      text-align: center
+      float left
+      box-sizing border-box
+      overflow hidden
+      text-align center
       a
-        display: block
-        width: 100%
-        overflow: hidden
-        text-decoration: none
+        display block
+        width 100%
+        overflow hidden
+        text-decoration none
       img
-        display: block
-        width: 100%
+        display block
+        width 100%
   .dots
-    position: absolute
-    right: 0
-    left: 0
-    bottom: 12px
-    text-align: center
-    font-size: 0
+    position absolute
+    right 0
+    left 0
+    bottom 12px
+    text-align center
+    font-size 0
     .dot
-      display: inline-block
-      margin: 0 4px
-      width: 8px
-      height: 8px
-      border-radius: 50%
+      display inline-block
+      margin 0 4px
+      width 8px
+      height 8px
+      border-radius 50%
       background: $color-text-l
       &.active
-        width: 20px
-        border-radius: 5px
-        background: $color-text-ll
-
+        width 20px
+        border-radius 5px
+        background $color-text-ll
 </style>
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,20 +1,93 @@
 <template>
   <div class="recommend" ref="recommend">
-    <scroll ref="scroll" class="recommend-content"></scroll>
+    <scroll ref="scroll" class="recommend-content" :data="discList">
+      <div>
+        <div class="slider-wrapper" ref="slider-wrapper" v-if="recommends.length">
+          <slider>
+            <div v-for="(item, index) in recommends" :key="index">
+              <a :href="item.linkUrl">
+                <img class="needsclick" @load="loadImage" :src="item.picUrl">
+              </a>
+            </div>
+          </slider>
+        </div>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script>
-import Scroll from 'base/scroll/scroll'
 import Slider from 'base/slider/slider'
+// import Loading from 'base/loading/loading'
+import Scroll from 'base/scroll/scroll'
+import {getRecommend, getDiscList} from 'api/recommend'
+// import {playlistMixin} from 'common/js/mixin'
+import {ERR_OK} from 'api/config'
+import {mapMutations} from 'vuex'
 
-export default{
+export default {
+  data () {
+    return {
+      recommends: [],
+      discList: []
+    }
+  },
+  created () {
+    this._getRecommend()
+
+    this._getDiscList()
+  },
+  methods: {
+    loadImage () {
+      if (!this.checkloaded) {
+        this.checkloaded = true
+        this.$refs.scroll.refresh()
+      }
+    },
+    selectItem (item) {
+      this.$router.push({
+        path: `/recommend/${item.dissid}`
+      })
+      this.setDisc(item)
+    },
+    _getRecommend () {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList () {
+      getDiscList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.discList = res.data.list
+        }
+      })
+    },
+    ...mapMutations({
+      setDisc: 'SET_DISC'
+    })
+  },
   components: {
+    Slider,
     Scroll
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+@import "~common/stylus/variable"
 
+.recommend
+  width 100%
+  position fixed
+  top 88px
+  bottom 0
+    .recommend-content
+      height 100%
+      overflow hidden
+      .slider-wrapper
+        position relative
+        width 100%
+        overflow hidden
 </style>
